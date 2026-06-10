d4fe1a89bc1aef3987f16762d66ab8a76511d425
User <oriana.flores@ucb.edu.bo>
2026-06-10 16:15:21 -0400

    feat: unitest

--- composeApp/src/commonTest/kotlin/truck/project/ComposeAppCommonTest.kt
+++ composeApp/src/commonTest/kotlin/truck/project/ComposeAppCommonTest.kt
@@ -3,6 +3,7 @@ package truck.project
 import truck.project.truck_guia.domain.vo.Placa
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class ComposeAppCommonTest {
 
@@ -12,5 +13,53 @@ class ComposeAppCommonTest {
         val expect = "ABC-1234"
         val placa = Placa.create(input)
         assertEquals(expect, placa.value)
+    }
+
+    @Test
+    fun `Placa debe eliminar espacios al inicio y al final`() {
+        // Arrange
+        val input = "   kmp-2026   "
+        val expect = "KMP-2026"
+
+        // Act
+        val placa = Placa.create(input)
+
+        // Assert
+        assertEquals(expect, placa.value)
+    }
+
+    @Test
+    fun `Placa debe convertir letras minusculas a mayusculas`() {
+        // Arrange
+        val input = "abc-5678"
+        val expect = "ABC-5678"
+
+        // Act
+        val placa = Placa.create(input)
+
+        // Assert
+        assertEquals(expect, placa.value)
+    }
+
+    @Test
+    fun `Placa no debe aceptar texto vacio`() {
+        // Arrange
+        val input = ""
+
+        // Act - Assert
+        assertFailsWith<IllegalArgumentException> {
+            Placa.create(input)
+        }
+    }
+
+    @Test
+    fun `Placa no debe aceptar solo espacios`() {
+        // Arrange
+        val input = "     "
+
+        // Act - Assert
+        assertFailsWith<IllegalArgumentException> {
+            Placa.create(input)
+        }
     }
 }
